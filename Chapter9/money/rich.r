150b1adbb904a759f52766cf9e92537f8b39192b
Masato setoyama <setoyama60jp@gmail.com>
2013-01-05 18:24:39 +0900

    9章のスクリプトでgridパッケージを読み込んでいないところを修正した。

--- Chapter9/money/rich.r
+++ Chapter9/money/rich.r
@@ -1,11 +1,14 @@
+library(grid)
 library(ggplot2)
 data <- read.table("money.csv", header=F, sep=",")
 samples <- data.frame(roid=1:51)
 points = c(1,5,15,30,50,75,100,125,150,200,300,500)
- pdf("money.pdf")
+
+pdf("money.pdf")
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(4,3)))
 vplayout <- function(x,y) {viewport(layout.pos.row=x, layout.pos.col=y)}
+
 row <- 1; col <- 1
 for (i in points) {
   point <- data.frame(t(data[i,]))
