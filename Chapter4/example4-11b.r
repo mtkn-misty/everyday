7bc52827930e45d2946897d7e0cad0ec659509e0
Masato setoyama <setoyama60jp@gmail.com>
2013-01-16 22:09:13 +0900

    figure4-8の頻度がグラフの外に出てしまっていたので、%を改行して表示するように修正。

--- Chapter4/example4-11b.r
+++ Chapter4/example4-11b.r
@@ -4,7 +4,7 @@ pdf("figure4-8.pdf")
 data <- read.table("simulation3-1.csv", header=TRUE, sep=",")
 df <- data.frame(table(data$X70))
 colnames(df) <- c("queue_size", "frequency")
-percent_labels <- paste(df$frequency, '(', round(df$frequency*100/540, 2), '%)')
+percent_labels <- paste(df$frequency, '\n(', round(df$frequency*100/540, 2), '%)')
 
 ggplot(data=df) + opts(legend.position = "none") + scale_fill_grey(start = 0.5, end = 0.8) + 
   geom_bar(aes(x = queue_size, y = frequency, fill = factor(queue_size))) +
